949a5f9e523f27e0c8d1e56bcd0610ce5171ad63
Guillaume Boucher <guiboucher8@gmail.com>
2025-09-22 15:42:31 -0400

    Create dependencies_packages.R

---
+++ dependencies_packages.R
@@ -0,0 +1,18 @@
+packages <- c(
+  "askpass",
+  "crayon",
+  "data.table",
+  "DT",
+  "lubridate",
+  "miniUI",
+  "odbc",
+  "roxygen2",
+  "shiny",
+  "shinyAce",
+  "shinydashboard",
+  "stringr",
+  "testthat",
+  "usethis",
+  "writexl"
+)
+for (p in packages) {install.packages(p)}
