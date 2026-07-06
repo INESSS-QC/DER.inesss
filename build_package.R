0b8c74e951dbce66f5f3ed07382d2c79209af743
Guillaume Boucher <guiboucher8@gmail.com>
2026-07-06 09:12:04 -0400

    Update build_package.R

--- build_package.R
+++ build_package.R
@@ -1,9 +1,9 @@
 # Répertoire commun - INESSS local
-if (file.exists("J:/GRP/A/5/A/Commun/0 Outils/Librairies R/fileExist")) {
-  devtools::build(path = "J:/GRP/A/5/A/Commun/0 Outils/Librairies R/DER.inesss.tar.gz")
+if (file.exists("J:/GRP/A/5/A/EMPLOYÉS/Commun/0 Outils/Librairies R/fileExist")) {
+  devtools::build(path = "J:/GRP/A/5/A/EMPLOYÉS/Commun/0 Outils/Librairies R/DER.inesss.tar.gz")
 }
 
 # Supprimer le fichier .Rhistory s'il existe
-if (file.exists("J:/GRP/A/5/A/Commun/0 Outils/Librairies R/.Rhistory")) {
-  file.remove("J:/GRP/A/5/A/Commun/0 Outils/Librairies R/.Rhistory")
+if (file.exists("J:/GRP/A/5/A/EMPLOYÉS/Commun/0 Outils/Librairies R/.Rhistory")) {
+  file.remove("J:/GRP/A/5/A/EMPLOYÉS/Commun/0 Outils/Librairies R/.Rhistory")
 }
